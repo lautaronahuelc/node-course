cb7ecbf6096ee60bb61bc482c8c0b50aefe42372
Lautaro Coria <lautaro.coria@valtech.com>
2024-05-10 22:18:41 -0300

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,16 @@
+// const fs = require('fs')
+// const getNotes = require('./notes')
+const chalk = require('chalk')
+
+
+// fs.writeFileSync('notes.txt', 'My name is Lautaro.')
+// challenge
+// fs.appendFileSync('notes.txt', ' I\u0027m 28 and I live in Buenos Aires, Argentina.')
+
+// challange
+// const notes = getNotes()
+// console.log(notes)
+
+console.log(chalk.red.inverse('Error!'))
+
+
