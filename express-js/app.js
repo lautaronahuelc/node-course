95aa1ca9f0d3aa047b0a852e79eeed0e5bd0d68e
lautaronahuelc <lautaronahuel.coria@gmail.com>
2024-06-30 01:32:46 -0300

    Update 2024_06_30

--- express-js/app.js
+++ express-js/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000); // It does http.createServer(this) internally
